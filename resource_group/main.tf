78f71302608e0cc6b09846da985c58fcef9670f5
ravidahiya8222 <ravidahiya789@gmail.com>
2025-06-29 15:52:12 +0530

    second chamge

--- resource_group/main.tf
+++ resource_group/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rj" {
     name = var.resource_group_name
     location = var.resource_group_location
 }
+
+resource "azurerm_resource_group" "rj2" {
+    name     = var.resource_group_name2
+    location = var.resource_group_location2
+}
